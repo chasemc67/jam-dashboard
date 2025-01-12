bb1916dcd22625234c6a0b33f589781b7a035231
chasemc67 <chasemc67@gmail.com>
2025-01-12 13:09:54 -0800

    Hide key picker until its hooked up

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,12 +17,12 @@ export default function Index() {
   return (
     <div className="min-h-screen bg-background dark">
       <div className="container mx-auto flex min-h-screen flex-col items-center justify-center gap-8 p-4">
-        <div className="w-full max-w-sm space-y-4 rounded-lg bg-card p-8 shadow-lg">
+        {/* <div className="w-full max-w-sm space-y-4 rounded-lg bg-card p-8 shadow-lg">
           <h1 className="text-2xl font-bold text-primary-foreground">
             Jam Dashboard
           </h1>
           <KeyPicker />
-        </div>
+        </div> */}
         <FretboardControls />
         <RandomPlayer />
       </div>
