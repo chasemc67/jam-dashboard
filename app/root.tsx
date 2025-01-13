0f49337273cbbd6f5763d00201e6eb1f6e3e5709
chasemc67 <chasemc67@gmail.com>
2025-01-12 21:00:31 -0800

    Add comment about overscroll

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
         <style
+          // disable overscroll
           dangerouslySetInnerHTML={{
             __html: `
           html, body {
