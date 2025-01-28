7ddb6cd4877ec8f458345dc09efa19c47e90a985
chasemc67 <chasemc67@gmail.com>
2025-01-28 14:17:15 -0800

    Fix padding on desktop

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import BuyMeCoffeeButton from '../BuyMeCoffeeButton';
 
 export default function Footer() {
   return (
-    <footer className="bg-background p-4">
-      <div className="container mx-auto">
-        <div className="flex justify-center md:justify-start">
+    <footer className="bg-background py-4 px-2">
+      <div className="mx-auto md:mr-8 md:ml-auto">
+        <div className="flex justify-center md:justify-end">
           <BuyMeCoffeeButton />
         </div>
       </div>
