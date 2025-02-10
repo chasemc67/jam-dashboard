e822ef9fe0e9f99f0c43fe6253a9afd068bb89ad
chasemc67 <chasemc67@gmail.com>
2025-02-10 13:00:52 -0800

    Turn CAGED back off so we can deploy

--- app/components/Fret/Fret.tsx
+++ app/components/Fret/Fret.tsx
@@ -55,7 +55,7 @@ const Fret: React.FC<FretProps> = ({
   fretNumber,
   highlightedNotes,
   showTextNotes,
-  isCaged = true,
+  isCaged = false,
   cagedShape = 'C',
 }) => {
   const widths = getFretWidth(fretNumber);
