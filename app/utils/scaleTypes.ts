8c720cea41e3938b4a5eaabb4e21d0b54221191f
chasemc67 <chasemc67@gmail.com>
2025-02-20 10:39:21 -0800

    Add scale types to include

---
+++ app/utils/scaleTypes.ts
@@ -0,0 +1,13 @@
+export const filteredScaleTypes = {
+  simple: ['major', 'minor'],
+  minors: ['harmonic minor', 'melodic minor'],
+  modes: [
+    'ionian',
+    'dorian',
+    'phrygian',
+    'lydian',
+    'mixolydian',
+    'aeolian',
+    'locrian',
+  ],
+};
